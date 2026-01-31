8a26ebea347179f7384af230ec9d88985fa3a54c
Azkaazeem <azkaazeem804@gmail.com>
2026-01-31 14:26:21 +0500

    complete

--- JavaScriptFiles/index.js
+++ JavaScriptFiles/index.js
@@ -206,7 +206,7 @@ async function signUp(e) {
                             phone: sPhn.value,
                             role: "user"
                         })
-                    window.location.href = "../UsersFiles/home.html"
+                    window.location.href = "../Usersfiles/home.html"
                 })
 
             if (error) {
